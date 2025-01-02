db77d8e67a1ca5d968c55a0756f2962165068ae1
adabugra <57899270+adabugra@users.noreply.github.com>
2025-01-02 21:25:06 +0300

    Add 1.21.4 support

--- src/main/java/net/elytrium/limboreconnect/LimboReconnect.java
+++ src/main/java/net/elytrium/limboreconnect/LimboReconnect.java
@@ -168,6 +168,7 @@ public class LimboReconnect {
             new PacketMapping(0x66, ProtocolVersion.MINECRAFT_1_20_3, true),
             new PacketMapping(0x68, ProtocolVersion.MINECRAFT_1_20_5, true),
             new PacketMapping(0x6f, ProtocolVersion.MINECRAFT_1_21_2, true),
+            new PacketMapping(0x6F, ProtocolVersion.MINECRAFT_1_21_4, true),
             });
 
     this.offlineTitles.clear();
